ef637bf657d0f85d49c3bde23cc275a3da0f252c
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-07-12 00:59:36 -0700

    first day with javascript

---
+++ hello.js
@@ -0,0 +1,42 @@
+console.log(21);
+console.log('Good Morning Javascript');
+var BananaPrice=12;
+console.log(BananaPrice);
+console.log(typeof BananaPrice);
+var name="Dhrubo";
+console.log(name);
+console.log(typeof name);
+var isHOT=true;
+var isRICH= false;
+console.log(isHOT);
+console.log(typeof isHOT);
+
+
+//Assignment
+
+var PotatoPrice=200;
+console.log(PotatoPrice);
+console.log(typeof PotatoPrice);
+
+var name="Solimuddin";
+console.log(name);
+console.log(typeof name);
+
+var isGOOD=true;
+var isBAD=false;
+console.log(isGOOD);
+console.log(typeof isGOOD);
+
+//finding everyting of a string in LOWERCASE.
+
+var promise="I WILL WORK VERY HARD TO BECOME A PROGRAMMER.";
+console.log(promise.toLowerCase())
+
+//finding everyting of a string in UPPERCASE.
+
+var tutorial="i start javascript programming today 12/06/2020 by programming hero tutorial of jhankar mahbub vaiya.";
+console.log(tutorial.toUpperCase())
+
+//Find the location of a expected string.
+
+console.log(tutorial.indexOf("jhankar"));
