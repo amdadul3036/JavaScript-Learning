9ade585a48ddb68fef0fc5c129bf0357f961c8b9
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-28 09:50:34 -0700

    reduce in array

---
+++ reduce.js
@@ -0,0 +1,6 @@
+// Reduce will work like school er classroom e khata uthanor moveTo. ekekjon dibe ar amar kache joma hobe. Then sob joma gula sir er table e diye asbo.
+
+const numbers = [12,54,3,5,7,8,2,90];
+
+const total = numbers.reduce((sum , num) => sum + num,0);
+console.log(total);
