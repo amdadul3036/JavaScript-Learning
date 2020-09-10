f7b684e9ffc5ae01ac4b1be81b905228424cdabf
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-10 09:18:34 -0700

    Ceiling the numbers

---
+++ ceilNumber.js
@@ -0,0 +1,20 @@
+var number1 = 54.8594;
+var number2 = 54.3245;
+var number3 = 54.5234;
+var number4 = 54.5034;
+var number5 = 54.5000;
+
+
+var ceilNumber1 = Math.ceil(number1);
+var ceilNumber2 = Math.ceil(number2);
+var ceilNumber3 = Math.ceil(number3);
+var ceilNumber4 = Math.ceil(number4);
+var ceilNumber5 = Math.ceil(number5);
+
+
+console.log(ceilNumber1);
+console.log(ceilNumber2);
+console.log(ceilNumber3);
+console.log(ceilNumber4);
+console.log(ceilNumber5);
+
