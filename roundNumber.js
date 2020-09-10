72bd6ff4032f423e9f9127369889ae824fb74a0d
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-10 09:15:20 -0700

    Rounding Math

---
+++ roundNumber.js
@@ -0,0 +1,20 @@
+var number1 = 54.8594;
+var number2 = 54.3245;
+var number3 = 54.5234;
+var number4 = 54.5034;
+var number5 = 54.5000;
+
+
+var roundNumber1 = Math.round(number1);
+var roundNumber2 = Math.round(number2);
+var roundNumber3 = Math.round(number3);
+var roundNumber4 = Math.round(number4);
+var roundNumber5 = Math.round(number5);
+
+
+console.log(roundNumber1);
+console.log(roundNumber2);
+console.log(roundNumber3);
+console.log(roundNumber4);
+console.log(roundNumber5);
+
