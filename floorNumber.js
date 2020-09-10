d5a48538a5cc4db0d398a8c6422b2d6a9c26eae4
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-10 09:21:11 -0700

    Floor the number

---
+++ floorNumber.js
@@ -0,0 +1,19 @@
+var number1 = 54.8594;
+var number2 = 54.3245;
+var number3 = 54.5234;
+var number4 = 54.5034;
+var number5 = 54.5000;
+
+
+var floorNumber1 = Math.floor(number1);
+var floorNumber2 = Math.floor(number2);
+var floorNumber3 = Math.floor(number3);
+var floorNumber4 = Math.floor(number4);
+var floorNumber5 = Math.floor(number5);
+
+
+console.log(floorNumber1);
+console.log(floorNumber2);
+console.log(floorNumber3);
+console.log(floorNumber4);
+console.log(floorNumber5);
