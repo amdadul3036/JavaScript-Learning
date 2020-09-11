fcb771990df38f023d6db6576e1aea6c3aed952b
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-10 21:38:19 -0700

    Slice

---
+++ sliceArray.js
@@ -0,0 +1,5 @@
+var array1 = ["Chagol", "Pagol" , "Dagol", "Magol" , "Jiboner goal" , "Amar goal" ,"Tomar goal" , "JavaScript Sekha"];
+
+var part = array1.slice(2,6);
+console.log(part); //Mane je porjonto nibo tar porer ta dibo(6) , ar jeta theke nibo seta dibo(2).
+console.log(array1); //Ete array1 er kono change hobe na; arra1 okkhoto thakbe.
