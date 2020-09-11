90381d5808da5de796bcb30be84b48837d0b6d21
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-11 06:25:09 -0700

    Large number in array

---
+++ max.js
@@ -0,0 +1,12 @@
+var array = [34,56,3,456,3256,78,43,453];
+
+var max = array[0];
+
+for(var i = 0 ; i<=array.length ; i++){
+    if(array[i]>max){
+        max = array[i];
+    }
+    else(max=max);
+}
+
+console.log(max);
