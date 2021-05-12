4910689d4c2b4b46147b237c0e0ecfab8810cb1c
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-05-12 09:14:02 +0600

    Variable in Javascript

--- .vscode/NEW JOURNEY/Variable/variable.js
+++ .vscode/NEW JOURNEY/Variable/variable.js
@@ -4,3 +4,14 @@ var B = 15;
 var C = A + B ;
 
 document.write(C);
+
+/*
+As JavaScript is the Dynamic Language, it do not required to indicate the type of variables
+It ditect automatically whither the variable is number(integer,float,double) or string or boolean.
+*/
+
+var D = "Dhrubo"; //string
+var E = true;     // Boolean
+
+document.write(D);
+document.write(E);
