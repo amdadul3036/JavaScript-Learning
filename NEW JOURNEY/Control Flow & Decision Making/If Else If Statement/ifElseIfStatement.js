48217da911224381a524fd4fc9866bf84d54c346
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-05-17 20:04:46 +0600

    If Else If Statement

---
+++ NEW JOURNEY/Control Flow & Decision Making/If Else If Statement/ifElseIfStatement.js
@@ -0,0 +1,13 @@
+var a =  3;
+
+if (a>3) {
+    alert("It seems greater than 3. Do you want to proceed ? If 'Yes' press 'OK'. ");
+    document.write("You've pressed 'OK'. Thank you for your effort.");
+}else if (a = 3){
+    alert("It seems equal 3. Do you want to proceed ? If 'Yes' press 'OK'. ");
+    document.write("You've pressed 'OK'. Thank you for your effort.");
+}
+else{
+    alert("It seems less than or equal 3. Do you want to proceed ? If 'Yes' press 'OK'. ");
+    document.write("You've pressed 'OK'. Thank you for your effort.");
+}
