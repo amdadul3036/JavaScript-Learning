6c672964d63bccbea5086300baa0cfd88ce86b7a
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-05-17 19:35:23 +0600

    If statement

---
+++ NEW JOURNEY/Control Flow & Decision Making/If Statement/ifStatement.js
@@ -0,0 +1,6 @@
+var a =  4;
+
+if (a>3) {
+    alert("It's seems greater than 3. Do you want to proceed ? If 'Yes' press 'OK'. ");
+    document.write("You've pressed 'OK'. Thank you for your effort.");
+}
