1ab903e82e9e3eacf440776bbd0c1a0d5c880a51
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-05-17 20:00:13 +0600

    If Else Statement

---
+++ NEW JOURNEY/Control Flow & Decision Making/If Else Statement/ifElseStatement.js
@@ -0,0 +1,9 @@
+var a =  2;
+
+if (a>3) {
+    alert("It's seems greater than 3. Do you want to proceed ? If 'Yes' press 'OK'. ");
+    document.write("You've pressed 'OK'. Thank you for your effort.");
+}else{
+    alert("It's seems less than or equal 3. Do you want to proceed ? If 'Yes' press 'OK'. ");
+    document.write("You've pressed 'OK'. Thank you for your effort.");
+}
