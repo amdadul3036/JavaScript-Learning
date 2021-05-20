e0712cb505ac16ec7fbb43bbb7297a51192a69ba
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-05-20 10:01:49 +0600

    onMouse Over trying

---
+++ NEW JOURNEY/Events/Mouse Over Event/onMouseOver.js
@@ -0,0 +1,9 @@
+function onMouseOverEvent(){
+    onmouseout("I am out");
+    onmousedown("I am down");
+    onmouseenter("I am Enter");
+    onmouseleave("I am Leave");
+    onmouseup("I am up");
+    onmousemove("I am move");
+    onmousewheel("I am wheel");
+}
