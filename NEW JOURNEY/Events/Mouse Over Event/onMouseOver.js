2dfafac37b4ed71501e32227544bde0650d5217e
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-05-20 10:29:49 +0600

    On mouse over is working

--- NEW JOURNEY/Events/Mouse Over Event/onMouseOver.js
+++ NEW JOURNEY/Events/Mouse Over Event/onMouseOver.js
@@ -1,9 +1,3 @@
 function onMouseOverEvent(){
-    onmouseout("I am out");
-    onmousedown("I am down");
-    onmouseenter("I am Enter");
-    onmouseleave("I am Leave");
-    onmouseup("I am up");
-    onmousemove("I am move");
-    onmousewheel("I am wheel");
+    document.getElementById("btn1").innerHTML = "Ok I have changed";
 }
