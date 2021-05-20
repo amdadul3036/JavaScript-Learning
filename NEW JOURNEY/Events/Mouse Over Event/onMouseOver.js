968f03b9c2bcaabb271590f07090f680c75d3416
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-05-20 10:32:12 +0600

    on Mouse Out Event working

--- NEW JOURNEY/Events/Mouse Over Event/onMouseOver.js
+++ NEW JOURNEY/Events/Mouse Over Event/onMouseOver.js
@@ -1,3 +1,7 @@
 function onMouseOverEvent(){
     document.getElementById("btn1").innerHTML = "Ok I have changed";
 }
+
+function onMouseOutEvent(){
+    document.getElementById("btn1").innerHTML = "On Mouse Over H1(Back Again)";
+}
