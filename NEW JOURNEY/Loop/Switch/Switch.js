6c04a9055097c6b5afdbaf4a699296f3a45907d2
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-05-27 12:07:33 +0600

    Switch

---
+++ NEW JOURNEY/Loop/Switch/Switch.js
@@ -0,0 +1,19 @@
+var grade = "e";
+
+switch (grade){
+    case "A":
+            document.write("Grade is A");
+            break;
+    
+    case "B":
+            document.write("Grade is B");
+            break;
+
+    case "C":
+            document.write("Grade is C");
+            break;
+
+    default:
+            document.write("FAIL");
+            break;
+}
