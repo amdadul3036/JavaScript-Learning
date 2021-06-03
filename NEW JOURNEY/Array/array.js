c045afe29cdc4692d38539f867b105854ae03188
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-06-03 23:01:36 +0600

    Part by part

--- NEW JOURNEY/Array/array.js
+++ NEW JOURNEY/Array/array.js
@@ -1,4 +1,12 @@
+// ---------------------Step 1 : Declare an Array -----------------------------
 var arr = ["Rahim","Karim","Sokina","Kutub Ali"];
+
+// ---------------------Step 2 : Printing the whole Array -----------------------------
+
 document.write(arr + "<br>");
 
+// ---------------------Step 3 : Printing a specific element of an array -----------------------------
+
 console.log(arr[2]);
+
+
