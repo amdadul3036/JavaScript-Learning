0a07d316781be6d01bd69afd237368b96144269d
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-06-04 23:32:38 +0600

    Step 7

--- NEW JOURNEY/Array/array.js
+++ NEW JOURNEY/Array/array.js
@@ -9,4 +9,19 @@ document.write(arr + "<br>");
 
 console.log(arr[2]);
 
+// ---------------------Step 4 : Adding a new element with an array -----------------------------
 
+arr.push("Sogir Uddin");
+document.write(arr + "<br>");
+
+// ---------------------Step 5 : Remove the last element from an array -----------------------------
+
+arr.pop();
+document.write(arr + "<br>");
+
+// ---------------------Step 6 : Remove any element from an array -----------------------------
+
+arr.splice(2,3);
+document.write(arr + "<br>");
+
+// ---------------------Step 7 : Remove any element from an array -----------------------------
