acc848649dcd19449368277507d9ac682a952f12
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-06-07 00:11:04 +0600

    Concat

--- NEW JOURNEY/Array/array.js
+++ NEW JOURNEY/Array/array.js
@@ -24,5 +24,13 @@ document.write(arr + "<br>");
 arr.splice(2,3);
 document.write(arr + "<br>");
 
-// ---------------------Step 7 : Sorting Array -----------------------------
+// ---------------------Step 7 : Length of Array -----------------------------
+
+document.write(arr.length + "<br>");   //It will give the total number of item in the array. Not the last index number.
+
+// ---------------------Step 8 : Join 2 Array in 1 Array -----------------------------
+
+var institution = ["IKG","SCA","NDC","DU"];
+document.write(arr.concat(institution));    //In this method the array you want to keep first will be in first then ".concat" and then the second array.
+
 
