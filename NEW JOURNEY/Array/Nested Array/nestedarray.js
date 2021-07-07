b73d13c7c6d6e2742111509f34716ec5af853ca8
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-07-07 08:55:41 +0600

    Nested Array

---
+++ NEW JOURNEY/Array/Nested Array/nestedarray.js
@@ -0,0 +1,7 @@
+let [a,b,c,d,e,f,veg] = [1,2,3,4,5,6,["Alu", "Sosha", "Potol"]];
+
+document.write(a + '</br>');
+const vegitable = veg;
+document.write(vegitable + '</br>');
+document.write(vegitable[1] + '</br>');
+
