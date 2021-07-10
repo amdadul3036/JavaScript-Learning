b73f666bdb23b1523bf35c27f0b729f33e995342
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-07-10 23:56:23 +0600

    Spread Parameter

--- NEW JOURNEY/Array/Rest and Spread/Spread Parameter/spread.js
+++ NEW JOURNEY/Array/Rest and Spread/Spread Parameter/spread.js
@@ -7,3 +7,5 @@ document.write("Second Array: " + copyOrClonefriendsNameArray + '</br>');
 //Now check if friendsNameArray and copyOrClonefriendsNameArray are equal or not  
 
 document.write(friendsNameArray == copyOrClonefriendsNameArray + '</br>' );  //False
+
+//As it is false so it's clear that they are not equal. They are just copy of each other but not equal.
