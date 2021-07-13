101082316e7ea4484f170d286f974bf1ec51cb8b
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-07-13 22:51:19 +0600

    Merge two array

---
+++ NEW JOURNEY/Array/Merge Two Array/merge.js
@@ -0,0 +1,6 @@
+const schoolFriends = ["Arun", "Rifat", "Mim", "Amit"];
+
+const CollageFriends = ["Momin","Ranbir", "Sohan","Rafi"];
+
+const schoolCollageFriends = [...schoolFriends,...CollageFriends];
+document.write(schoolCollageFriends);
